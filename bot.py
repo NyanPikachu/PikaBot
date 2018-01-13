172c6da34bf61ae834f94aeb4f94ff479419c49d
NyanPikachu <32774211+NyanPikachu@users.noreply.github.com>
2018-01-13 16:19:07 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,9 +5,73 @@ import pickle
 import random
 import os
 import sys
+import traceback
+import textwrap
+from contextlib import redirect_stdout
 
+#eval!!!
+def cleanup_code(content):
+    """Automatically removes code blocks from the code."""
+    # remove ```py\n```
+    if content.startswith('```') and content.endswith('```'):
+        return '\n'.join(content.split('\n')[1:-1])
+
+    return content.strip('` \n')
 bot = commands.Bot(command_prefix="$", description="This is an example bot", owner_id=279974491071709194)
 
+devs = [
+    279974491071709194
+]
+
+@bot.command(hidden=True, name='eval')
+@commands.is_owner()
+async def _eval(ctx, *, body: str):
+        """Evaluates a code"""
+
+        env = {
+            'bot': bot,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+        }
+
+        env.update(globals())
+
+        body = cleanup_code(body)
+        stdout = io.StringIO()
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            try:
+                await ctx.message.add_reaction('\u2705')
+            except:
+                pass
+
+            if ret is None:
+                if value:
+                    await ctx.send(f'```py\n{value}\n```')
+            else:
+                await ctx.send(f'```py\n{value}{ret}\n```')
+                
+
+
+#normal commands.ill put into cogs later
 @bot.event
 async def on_ready():
     print("Bot is online!")
