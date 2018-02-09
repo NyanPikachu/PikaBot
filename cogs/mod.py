c1af53d5471dc618014c8b09273c88fe2d5eec91
NyanPikachu <32774211+NyanPikachu@users.noreply.github.com>
2018-02-09 15:08:55 +0100

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -9,14 +9,13 @@ class mod:
         
     @commands.command()
     @commands.has_permissions(kick_members=True)
-    async def kick(self, ctx ,user: discord.Member=None):
+    async def kick(self, ctx ,user: discord.Member=None, reason: str):
         '''Kick a member from the server!'''
         if not user:
             await ctx.send("Please mention a member for this command to work")
         try:
             embed = discord.Embed(title="Kick".format(user.name), description=f"{ctx.guild.name}", color=0xff0000)
-            embed.add_field(f"{ctx.author} has kicked {user.name}")
-            embed.add_field(f"reason:" + reason) 
+            embed.add_field(name=f"{ctx.author} has kicked {user.name}", value=reason) 
             embed.set_thumbnail(url=user.avatar_url)
             await ctx.guild.kick(user)
         except discord.Forbidden:
@@ -24,14 +23,13 @@ class mod:
     
     @commands.command()
     @commands.has_permissions(ban_members=True)
-    async def ban(self, ctx ,user: discord.Member=None):
+    async def ban(self, ctx ,user: discord.Member=None, reason: str):
         '''Ban a member from the server!'''
         if not user:
             await ctx.send("Please mention a member for this command to work")
         try:
             embed = discord.Embed(title="Ban".format(user.name), description=f"{ctx.guild.name}", color=0xff0000)
-            embed.add_field(f"{ctx.author} has banned {user.name}")
-            embed.add_field(f"reason:" + reason) 
+            embed.add_field(name=f"{ctx.author} has banned {user.name}", value=reason) 
             embed.set_thumbnail(url=user.avatar_url)
             await ctx.guild.ban(user)
         except discord.Forbidden:
