aea0b0ca4fb8e50ad734a19d44d8bdc297017c87
NyanPikachu <32774211+NyanPikachu@users.noreply.github.com>
2018-02-13 13:55:50 +0100

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -33,7 +33,7 @@ class misc:
     @commands.command()
     async def embedsay(self, ctx, *, body: str):
         '''Send a simple embed'''
-        em = discord.Embed(description=body, color=discord.member.Colour)
+        em = discord.Embed(description=body, color=member.Colour)
         await ctx.send(embed=em)
         
     @commands.command(name='presence', hidden=True)
