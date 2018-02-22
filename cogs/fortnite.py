c4e2173464fb3633bba2d8b41de8727185235c12
NyanPikachu <32774211+NyanPikachu@users.noreply.github.com>
2018-02-23 00:01:01 +0100

    Create fortnite.py

---
+++ cogs/fortnite.py
@@ -0,0 +1,53 @@
+import discord
+from discord.ext import commands
+from ext.paginator import PaginatorSession
+
+class Fortnite:
+    '''Get Fortnite stats by name'''
+
+    def __init__(self, bot):
+        self.bot = bot
+        
+    client = pynite.Client('FNAPI')
+        
+    @commands.command
+    async def fnprofile(self, ctx, plat=None, name=None):
+        pages = []
+        
+        player = client.get_player(plat, name)
+        lifetime = await player.get_lifetime_stats()
+        
+        embed = discord.Embed(color=discord.Color.green())
+        embed.title = 'Name: ' + name
+        embed.description = 'Platform: ' + plat
+        embed.add_field(name=lifetime[9].key, value=lifetime[9].value)
+        embed.add_field(name=lifetime[1].key, value=lifetime[1].value)
+        embed.add_field(name=lifetime[4].key, value=lifetime[4].value)
+        embed.add_field(name=lifetime[6].key, value=lifetime[6].value)
+        embed.add_field(name=lifetime[7].key, value=lifetime[7].value)
+        embed.add_field(name=lifetime[13].key, value=lifetime[13].value)
+        embed.add_field(name=lifetime[10].key, value=lifetime[10].value)
+        embed.add_field(name=lifetime[11].key, value=lifetime[11].value)
+        embed.add_field(name=lifetime[12].key, value=lifetime[12].value)
+        
+        pages.append(embed)
+        
+        embed = discord.Embed(color=discord.Color.green())
+        embed.title = 'Name: ' + name
+        embed.description = 'Platform: ' + plat
+        embed.add_field(name='Victory Royales', value=solo.top1.value)
+        embed.add_field(name='Top 10', value=solo.top10.value)
+        embed.add_field(name='Score', value=solo.score.value)
+        embed.add_field(name='K/D', value=solo.kd.value)
+        embed.add_field(name='Kills', value=solo.kills.value)
+        embed.add_field(name='Matches Played', value=solo.matches.value)
+        embed.add_field(name='Time Played', value=solo.minutes_played.value)
+        
+        pages.append(embed)
+        
+        p_session = PaginatorSession(ctx, footer=f'Stats made by Cree-Py | Powered by fortnitetracker.com', pages=pages)
+        await p_session.run()
+        
+        
+def setup(bot):
+    bot.add_cog(Fortnite(bot))
