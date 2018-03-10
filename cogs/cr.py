53c5138cd074c5b3c029e229c8aa730bb61337ee
NyanPikachu <32774211+NyanPikachu@users.noreply.github.com>
2018-03-10 23:24:17 +0100

    Update cr.py

--- cogs/cr.py
+++ cogs/cr.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from ext.paginator import PaginatorSession
-import aiohttp
+import requests
 
 class Clash_Royale:
     '''Clash Royale commands to get your fancy stats here!'''
@@ -13,12 +13,14 @@ class Clash_Royale:
         '''Gets your Clash Royale Profile using Tag!'''
         if not tag:
             return await ctx.send('Please provide a tag for this command to work `Usage : $crprofile [tag]`')
-        headers = {"auth":"c94d84443b5345d784418332e81a5d3b272f67619a6b45368f2cbe5f064d3d55"}
-        async with aiohttp.ClientSession as session:
-            async with session.get("https://api.cr-api.com/players/{tag}", headers=headers) as resp:
-                data = await resp.json()
-                embed = discord.Embed(name=ctx.author.name)
-                embed.add_field(name='Name', value=data['name'])
+        headers = {
+        	"auth":"c94d84443b5345d784418332e81a5d3b272f67619a6b45368f2cbe5f064d3d55"
+       	}
+        r = requests.get(f"https://api.cr-api.com/players/{tag}", headers=headers)
+        cr = r.json()
+        embed = discord.Embed(name=ctx.author.name)
+        embed.add_field(name='Name', value=cr['name'])
+        asyncio.get_event_loop().run_until_complete(main())
         
 def setup(bot):
     bot.add_cog(Clash_Royale(bot))
