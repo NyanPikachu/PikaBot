201dece2e4827b8abcd371cab2bb8d56e83543a4
NyanPikachu <youssefjarray8@gmail.com>
2018-03-28 20:30:15 +0100

    /

--- cogs/cr.py
+++ cogs/cr.py
@@ -3,29 +3,29 @@ from discord.ext import commands
 from ext.paginator import PaginatorSession
 import clashroyale
 import os
-import aiofirebase
+from firebase import Firebase
 
 class Clash_Royale:
     '''Clash Royale commands to get your fancy stats here!'''
     def __init__(self, bot):
         self.bot = bot
-        self.firebase = aiofirebase.FirebaseHTTP("https://pikabot-fa952.firebaseio.com/", auth='AIzaSyB48L7AFMTepuFkvoFIcauJLJrrmVaRrVQ')
-         
+        self.db = Firebase('https://pikabot-fa952.firebaseio.com/', auth_token="AIzaSyB48L7AFMTepuFkvoFIcauJLJrrmVaRrVQ")
+        
     @commands.command()
     async def crsave(self, ctx, tag :str=None):
         if not tag:
             await ctx.send('Please provide a tag')
         try:
-           await self.firebase.put(path='clashroyale', value={ctx.author.id: tag})
+           f.push({'clashroyale': {str(ctx.author.id): tag}})
            await ctx.send('Tag successfully saved!. Run this command again to replace with another tag.')
         except Exception as e:
            await ctx.send(str(e))
-         
+        
     @commands.command()
     async def crprofile(self, ctx, tag: str=None):
         '''Gets your Clash Royale Profile using Tag!'''
         if not tag:
-            tag = await self.firebase.get(path='clash royale', value=ctx.author.id)
+            tag = f.get({'clashroyale': {str(ctx.author.id)}})
         token = os.environ.get("CRTOKEN")
         client = clashroyale.Client(token, is_async=True)
         profile = await client.get_player(tag)
