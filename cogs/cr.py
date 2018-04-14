e226bcd9fa7d1535998514de644eadd6e0e77245
NyanPikachu <youssefjarray8@gmail.com>
2018-04-14 19:15:45 +0200

    updated cr cog, again

--- cogs/cr.py
+++ cogs/cr.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from ext.paginator import PaginatorSession
 import clashroyale
 import os
+import json
 
 class Clash_Royale:
     '''Clash Royale commands to get your fancy stats here!'''
@@ -16,19 +17,52 @@ class Clash_Royale:
         '''Gets your Clash Royale Profile using Tag!'''
         if not tag:
             return await ctx.send('Please provide a tag for this command')
-            
-        profile = await self.client.get_player(tag)
-        clan = await profile.get_clan()
-        em = discord.Embed(color=discord.Color.gold())
-        em.title = profile.name
-        em.description = f'{tag}\'s info'
-        em.add_field(name='Favourite card:', value=profile.stats.favorite_card.name)
-        em.add_field(name='clan name', value=clan.name)
-        em.add_field(name='clan tag', value=clan.tag)
-        em.add_field(name='clan type', value=clan.type)
-        em.add_field(name='clan donations', value=clan.donations)
-        em.add_field(name='clan members', value=clan.memberCount)
-        await ctx.send(embed=em)
-		
+        
+        hasProfile = True
+        hasClan = True
+
+        if hasProfile:
+            profile = await self.client.get_player(tag)
+        except Exception as e:
+            return await ctx.send(f'```py\nError {e.code}: {e.error}\n```')
+        try:
+            clan = await profile.get_clan()
+        except Exception as e:
+            hasClan = False
+
+        pages = []
+
+        if hasProfile:
+            em = discord.Embed(color=discord.Color.gold())
+            em.title = profile.name
+            em.description = f'{tag}\'s info'
+            em.add_field(name='Trophies', value=profile.trophies)
+            em.add_field(name='Max Trophies', value=profile.stats.maxTrophies)
+            em.add_field(name='Arena', value=profile.arena.name)
+            em.add_field(name='League', value=profile.arena.arena)
+            em.add_field(name='Rank', value=profile.rank)
+            em.add_field(name='Wins', value=profile.games.wins)
+            em.add_field(name='Wins', value=profile.games.wins)
+            em.add_field(name='Losses', value=profile.games.losses)
+            em.add_field(name='Draws', value=profile.games.draws)
+            em.add_field(name='Cards Found', value=profile.stats.cardsFound)
+            em.add_field(name='Favourite card:', value=profile.stats.favorite_card.name)
+            pages.append(em)
+
+        if hasClan:
+            em = discord.Embed(color=discord.Color.gold())
+            em.title = clan.name
+            em.thumbnail = clan.badge.image
+            em.add_field(name='Name', value=clan.name)
+            em.add_field(name='Role', value=clan.role)
+            em.add_field(name='Tag', value=clan.tag)
+            em.add_field(name='Type', value=clan.type)
+            em.add_field(name='Donations', value=clan.donations)
+            em.add_field(name='Members', value=clan.memberCount)
+            pages.append(em)
+
+        p_session = PaginatorSession(ctx, footer=f'Bot made by Nyan Pikachu#4148 | Wrapper provided by CodeGrok, API by RoyaleAPI', pages=pages)
+        await p_session.run()
+
 def setup(bot):
     bot.add_cog(Clash_Royale(bot))
--- cogs/fortnite.py
+++ cogs/fortnite.py
@@ -8,11 +8,13 @@ class Fortnite:
 
     def __init__(self, bot):
         self.bot = bot
+        self.client = pynite.Client('5a20baea-b8a7-4e42-9de3-741534219452')
        
-        
-    @commands.command()
+    @commands.command(aliases=[])
     async def fnprofile(self, ctx, plat=None, *, name=None):
         '''Get stats for your fortnite account !'''
+        plat =  plat.lower()
+        
         if name is None:
             await ctx.send("Please specify a username as well as the platform (defaults to pc if not specified).")
             return
@@ -21,67 +23,84 @@ class Fortnite:
             await ctx.send("Invalid platform, platforms are {psn, xbl, pc}.")
             return
 
+        hasSolos = True
+        hasDuos = True
+        hasSquads = True
+        hasLifetime = True
+
+        try:
+            player = await self.client.get_player(plat, name)
+        except Exception as e:
+            return await ctx.send(f'```py\nError {e.code}: {e.error}\n```')
+        try:
+            solo = await player.get_solos()
+        except Exception as e:
+            hasSolos = False
+        try:
+            duos = await player.get_duos()
+        except Exception as e:
+            hasDuos = False
+        try:
+            lifetime = await player.get_lifetime_stats()
+        except Exception as e:
+            hasLifetime = False
+            
         pages = []
-        client = pynite.Client('5a20baea-b8a7-4e42-9de3-741534219452')
-        player = await client.get_player(plat, name)
-        lifetime = await player.get_lifetime_stats()
-        solo = await player.get_solos()
-        duos = await player.get_duos()
-        squads = await player.get_squads()
+
+        if hasLifetime:
+            embed = discord.Embed(color=discord.Color.blue())
+            embed.title = 'Name: ' + player.epic_user_handle + ' - LifeTime stats'
+            embed.description = 'Platform: ' + player.platform_name_long
+            embed.add_field(name=lifetime[9].key, value=lifetime[9].value)
+            embed.add_field(name=lifetime[1].key, value=lifetime[1].value)
+            embed.add_field(name=lifetime[4].key, value=lifetime[4].value)
+            embed.add_field(name=lifetime[6].key, value=lifetime[6].value)
+            embed.add_field(name=lifetime[7].key, value=lifetime[7].value)
+            embed.add_field(name=lifetime[13].key, value=lifetime[13].value)
+            embed.add_field(name=lifetime[10].key, value=lifetime[10].value)
+            embed.add_field(name=lifetime[11].key, value=lifetime[11].value)
+            embed.add_field(name=lifetime[12].key, value=lifetime[12].value)
+            pages.append(embed)
         
-        embed = discord.Embed(color=discord.Color.blue())
-        embed.title = 'Name: ' + player.epic_user_handle + ' - LifeTime stats'
-        embed.description = 'Platform: ' + player.platform_name_long
-        embed.add_field(name=lifetime[9].key, value=lifetime[9].value)
-        embed.add_field(name=lifetime[1].key, value=lifetime[1].value)
-        embed.add_field(name=lifetime[4].key, value=lifetime[4].value)
-        embed.add_field(name=lifetime[6].key, value=lifetime[6].value)
-        embed.add_field(name=lifetime[7].key, value=lifetime[7].value)
-        embed.add_field(name=lifetime[13].key, value=lifetime[13].value)
-        embed.add_field(name=lifetime[10].key, value=lifetime[10].value)
-        embed.add_field(name=lifetime[11].key, value=lifetime[11].value)
-        embed.add_field(name=lifetime[12].key, value=lifetime[12].value)
+        if hasSolos:
+            embed = discord.Embed(color=discord.Color.blue())
+            embed.title = 'Name: ' + player.epic_user_handle + ' - Solo Stats'
+            embed.description = 'Platform: ' + player.platform_name_long
+            embed.add_field(name='Victory Royales', value=solo.top1.value)
+            embed.add_field(name='Top 10', value=solo.top10.value)
+            embed.add_field(name='Score', value=solo.score.value)
+            embed.add_field(name='K/D', value=solo.kd.value)
+            embed.add_field(name='Kills', value=solo.kills.value)
+            embed.add_field(name='Average kills per match', value=solo.kpg.value)
+            embed.add_field(name='Matches Played', value=solo.matches.value)
+            pages.append(embed)
         
-        pages.append(embed)
+        if hasDuos:
+            embed = discord.Embed(color=discord.Color.blue())
+            embed.title = 'Name: ' + player.epic_user_handle + ' - Duo Stats'
+            embed.description = 'Platform: ' + player.platform_name_long
+            embed.add_field(name='Victory Royales', value=solo.top1.value)
+            embed.add_field(name='Top 10', value=duos.top10.value)
+            embed.add_field(name='Score', value=duos.score.value)
+            embed.add_field(name='K/D', value=duos.kd.value)
+            embed.add_field(name='Kills', value=duos.kills.value)
+            embed.add_field(name='Average kills per match', value=duos.kpg.value)
+            embed.add_field(name='Matches Played', value=duos.matches.value)
+            pages.append(embed)
         
-        embed = discord.Embed(color=discord.Color.blue())
-        embed.title = 'Name: ' + player.epic_user_handle + ' - Solo Stats'
-        embed.description = 'Platform: ' + player.platform_name_long
-        embed.add_field(name='Victory Royales', value=solo.top1.value)
-        embed.add_field(name='Top 10', value=solo.top10.value)
-        embed.add_field(name='Score', value=solo.score.value)
-        embed.add_field(name='K/D', value=solo.kd.value)
-        embed.add_field(name='Kills', value=solo.kills.value)
-        embed.add_field(name='Average kills per match', value=solo.kpg.value)
-        embed.add_field(name='Matches Played', value=solo.matches.value)
-        
-        pages.append(embed)
-        
-        embed = discord.Embed(color=discord.Color.blue())
-        embed.title = 'Name: ' + player.epic_user_handle + ' - Duo Stats'
-        embed.description = 'Platform: ' + player.platform_name_long
-        embed.add_field(name='Victory Royales', value=solo.top1.value)
-        embed.add_field(name='Top 10', value=duos.top10.value)
-        embed.add_field(name='Score', value=duos.score.value)
-        embed.add_field(name='K/D', value=duos.kd.value)
-        embed.add_field(name='Kills', value=duos.kills.value)
-        embed.add_field(name='Average kills per match', value=duos.kpg.value)
-        embed.add_field(name='Matches Played', value=duos.matches.value)
-        
-        pages.append(embed)
-        
-        embed = discord.Embed(color=discord.Color.blue())
-        embed.title = 'Name: ' + player.epic_user_handle + ' - Squad Stats'
-        embed.description = 'Platform: ' + player.platform_name_long
-        embed.add_field(name='Victory Royales', value=solo.top1.value)
-        embed.add_field(name='Top 10', value=squads.top10.value)
-        embed.add_field(name='Score', value=squads.score.value)
-        embed.add_field(name='K/D', value=squads.kd.value)
-        embed.add_field(name='Kills', value=squads.kills.value)
-        embed.add_field(name='Average kills per match', value=squads.kpg.value)
-        embed.add_field(name='Matches Played', value=squads.matches.value)
-        
-        pages.append(embed)
+        if hasSquads:
+            embed = discord.Embed(color=discord.Color.blue())
+            embed.title = 'Name: ' + player.epic_user_handle + ' - Squad Stats'
+            embed.description = 'Platform: ' + player.platform_name_long
+            embed.add_field(name='Victory Royales', value=squads.top1.value)
+            embed.add_field(name='Top 10', value=squads.top10.value)
+            embed.add_field(name='Score', value=squads.score.value)
+            embed.add_field(name='K/D', value=squads.kd.value)
+            embed.add_field(name='Kills', value=squads.kills.value)
+            embed.add_field(name='Average kills per match', value=squads.kpg.value)
+            embed.add_field(name='Matches Played', value=squads.matches.value)
+            pages.append(embed)
+
         p_session = PaginatorSession(ctx, footer=f'Bot made by Nyan Pikachu#4148 | Powered by fortnitetracker.com', pages=pages)
         await p_session.run()
         
