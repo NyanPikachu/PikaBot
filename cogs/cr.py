5651c905405d7e22624c0cf138fbe2b932aa621b
NyanPikachu <youssefjarray8@gmail.com>
2018-04-17 13:46:43 +0200

    lol

--- cogs/cr.py
+++ cogs/cr.py
@@ -12,7 +12,7 @@ class Clash_Royale:
         self.bot = bot
         self.token = (os.environ.get('CRTOKEN'))
         self.client = clashroyale.Client(self.token, is_async=True)
-        self.dbclient = motor_asyncio.AsyncIOMotorClient('mongodb://Nyan Pikachu:' + os.environ.get('DBPASS') + '@ds163711.mlab.com:63711/pikabot')
+        self.dbclient = motor_asyncio.AsyncIOMotorClient('mongodb://Nyan Pikachu:' + os.environ.get('DBPASS') + '@ds163711.mlab.com:63711')
         self.db = self.dbclient.pikabot
 
     def check_tag(self, tag):
