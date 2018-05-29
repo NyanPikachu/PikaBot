9a030033c9a6e37708a64145400dc98280637d43
NyanPikachu <youssefjarray8@gmail.com>
2018-05-30 01:01:34 +0200

    k

--- cogs/level_system.py
+++ cogs/level_system.py
@@ -14,30 +14,30 @@ class xp:
     async def update_xp(self, xp: int, guildID, userID):
         await self.db.xp.update_one({'_id': guildID}, {'$set': {'_id': guildID, userID: xp}}, upsert=True)
 
-	async def get_xp(self, guildID, userID):
-    	try:
-    		result = await self.db.xp.find_one({'_id': userID})
-    	except Exception as e:
-    		er = f'error : `{e}`'
-    		return er
-    	if not result or not result.get('userID'):
-    		return "Not Found"
-    	return result['userID']
+    async def get_xp(self, guildID, userID):
+        try:
+            result = await self.db.xp.find_one({'_id': userID})
+        except Exception as e:
+            er = f'error : `{e}`'
+            return er
+        if not result or not result.get('userID'):
+            return "Not Found"
+        return result['userID']
 
     async def on_message(self, message):
-    	ch = message.channel
-    	guild = str(message.guild.id)
-    	user = str(message.author.id)
-		new_xp = self.get_xp(guild, user) + 15
+        ch = message.channel
+        guild = str(message.guild.id)
+        user = str(message.author.id)
+        new_xp = self.get_xp(guild, user) + 15
 
-    	if message.author.bot:
-    		return
-    	try:
-    		await self.update_xp(new_xp, guild, user)
-    	except Exception as e:
-    		await ch.send(e)
+        if message.author.bot:
+            return
+        try:
+            await self.update_xp(new_xp, guild, user)
+        except Exception as e:
+            await ch.send(e)
 
-    	await self.bot.process_commands(message)
+        await self.bot.process_commands(message)
 
 def setup(bot):
     bot.add_cog(xp(bot))
